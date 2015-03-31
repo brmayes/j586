dd041111daecacccc06a22659fa6cd29330e867e
Mayes <brmayes@jomc7952.dhcp.unc.edu>
2015-03-31 00:35:46 -0400

    NAV IS DONE TURN UP

--- project folders/project-three/js/dist/js/slidermenu.js
+++ project folders/project-three/js/dist/js/slidermenu.js
@@ -1,12 +1,28 @@
-			$(function() {
-				$('nav#menu').mmenu({
-					extensions	: [ 'effect-slide', 'pageshadow' ],
-					header		: true,
-					searchfield	: true,
-					counters	: true,
-					footer		: {
-						add			: true,
-						content		: '<a href="mmenu.frebsite.nl">Visit website &rsaquo;</a>'
-					}
-				});
-			});
+$(function() {
+    
+                    var month2 = new Date();
+                    var month2Array = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+                    document.getElementById("month2").innerHTML = month2Array[month2.getMonth()];
+                    
+                    var day2 = new Date();
+                    document.getElementById("day2").innerHTML = day2.getDate();
+                    
+                    var years2 = new Date();
+                    document.getElementById("years2").innerHTML = years2.getFullYear();
+    
+        $('nav#menu').mmenu({
+                extensions	: [ 'effect-slide', 'pageshadow' ],
+                header		: true,
+                searchfield	: true,
+                counters	: true,
+                footer		: {
+                        add			: false,
+                        content		: '<p><span id="month2"></span> <span id="day2"></span>, <span id="years2"></span></p>'
+                }
+        });
+});
+
+
+
+
+
