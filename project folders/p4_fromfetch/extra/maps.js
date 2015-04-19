7aa3457cb8e74c0a21ed3ec908fe533a550f08e4
Mayes <brmayes@jomc7885.dhcp.unc.edu>
2015-04-19 00:42:47 -0400

    need to figure out how to replace divs but added map that uses mark divs

---
+++ project folders/p4_fromfetch/extra/maps.js
@@ -0,0 +1,104 @@
+var infowindow;
+var styles = [
+{
+  stylers: [
+  { hue: "#00ffe6" },
+  { saturation: -20 }
+  ]
+},{
+  featureType: "road",
+  elementType: "geometry",
+  stylers: [
+  { lightness: 100 },
+  { visibility: "simplified" }
+  ]
+},{
+  featureType: "road",
+  elementType: "labels",
+  stylers: [
+  { visibility: "off" }
+  ]
+}
+];
+
+//map.setOptions({styles: styles});
+
+
+(function() {
+
+  google.maps.Map.prototype.markers = new Array();
+
+  google.maps.Map.prototype.addMarker = function(marker) {
+    this.markers[this.markers.length] = marker;
+  };
+
+  google.maps.Map.prototype.getMarkers = function() {
+    return this.markers
+  };
+
+  google.maps.Map.prototype.clearMarkers = function() {
+    if (infowindow) {
+      infowindow.close();
+    }
+
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].set_map(null);
+    }
+  };
+
+  map.setOptions({styles: styles});
+})();
+
+function initialize() {
+  var latlng = new google.maps.LatLng(35.912548, -79.050964);
+  var myOptions = {
+    zoom: 14,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    //mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+
+
+
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+
+
+  var a = new Array();
+  var t = new Object();
+  t.name = "test0"
+  t.lat = 35.912548
+  t.lng = -79.050964
+  a[0] = t;
+
+  var t = new Object();
+  t.name = "test1"
+  t.lat = 35.910797
+  t.lng = -79.050535
+  a[1] = t;
+
+
+
+  for (var i = 0; i < a.length; i++) {
+    var latlng = new google.maps.LatLng(a[i].lat, a[i].lng);
+    map.addMarker(createMarker(a[i].name, latlng));
+  }
+  console.log(map.getMarkers());
+
+  console.log(map.getMarkers());
+}
+
+function createMarker(name, latlng) {
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
+  google.maps.event.addListener(marker, "click", function() {
+    if (infowindow) infowindow.close();
+    infowindow = new google.maps.InfoWindow({
+      content: name
+    });
+    infowindow.open(map, marker);
+  });
+  return marker;
+}
--- project folders/project-four/google-maps/maps-trial.js
+++ project folders/project-four/google-maps/maps-trial.js
@@ -1,14 +1,34 @@
 function initialize() {
+
+  //variables for locations on map
+  var chapelHill = new google.maps.LatLng(35.912548,-79.050964);
+
+  //sets up map
   var map = new google.maps.Map(document.getElementById('map-trial'), {
     center: new google.maps.LatLng(40, -85),
     zoom: 4
   });
-  var marker = new google.maps.Marker({
+
+  //creates marker
+  var trayvon = new google.maps.Marker({
     position: map.getCenter(),
     map: map
   });
 
-  google.maps.event.addListener(marker, 'click', function (marker, i) {
+  var point2 = new google.maps.Marker({
+    position: chapelHill,
+    map: map
+  });
+
+  google.maps.event.addListener(trayvon, 'click', function (trayvon, i) {
+    if ($('#tmartin').css('display') == 'block') {
+      $('#tmartin').css('display', 'none');
+    } else {
+      $('#tmartin').css('display', 'block');
+    }
+  });
+
+  google.maps.event.addListener(point2, 'click', function (point2, i) {
     if ($('#info-container').css('display') == 'block') {
       $('#info-container').css('display', 'none');
     } else {
